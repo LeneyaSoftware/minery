9a5837dac7307f3a84712fca19d635d8be6cf8d4
Josh Walker <josh.walker@leneya.com>
2015-06-01 10:42:37 -0400

    update comments on the report base classes

--- src/Minery/Dig/ReportBase.php
+++ src/Minery/Dig/ReportBase.php
@@ -13,6 +13,7 @@ namespace Minery\Dig;
 
 use Minery\Dig\Contracts\iReport;
 use Minery\Dig\Contracts\ResultSetContract;
+use Minery\Sift\Filters\FilterCollection\FilterCollection;
 
 abstract class ReportBase implements iReport{
 
@@ -22,17 +23,30 @@ abstract class ReportBase implements iReport{
      * Creates a Report. Takes in an empty report result set that needs to be filled in when the report has completed
      * running.
      * @param iResultSet $emptyResultSet
+     * @param $db -- the database object that you want to use to search. Could be PDO, could be plain MYSQLi Connection.
+     *               Just the thing that you want to do the searching on.
+     * @param FilterCollection $filterCollection -- the filters you want to apply
      */
-    public function __construct(iResultSet $emptyResultSet){
+    public function __construct(iResultSet $emptyResultSet,$db = '',FilterCollection $filterCollection=null){
 
     }
 
     /**
-     * Runs this report.
-     * @param $db -- the database object you want to be searching on. Could be a query builder, could be PDO. Just pop it in and do work.
+     * Runs this report. Make sure to return a result that is consistent with the following structure.
+     *
+     *[
+     *  0 => ['field1' => 'value1' , 'field2' => 'value2'],
+     *  1 => ['field1' => 'value1' , 'field2' => 'value2'],
+     *  ...
+     *]
+     *
+     * Where field1 and field2 are the names of the columns you are returning and value1 / value2 are the values
+     * for those fields. Minery takes those fields and creates your headers for whatever result you are rendering,
+     * so they must be there.
+     *
      * @return mixed
      */
-    public abstract function run($db);
+    public abstract function run();
 
 
     /**
